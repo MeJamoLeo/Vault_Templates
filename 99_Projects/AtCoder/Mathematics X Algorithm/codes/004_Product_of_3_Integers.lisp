ef9fe4288777ea680ddc857183a66695c12abc67
treo <treo@treo.treo>
2025-03-08 07:06:43 -0600

    vault backup: 2025-03-08 07:06:43

--- 99_Projects/AtCoder/Mathematics X Algorithm/codes/004_Product_of_3_Integers.lisp
+++ 99_Projects/AtCoder/Mathematics X Algorithm/codes/004_Product_of_3_Integers.lisp
@@ -1,1 +1,6 @@
-(format t "~a~%" (* (read) (read) (read)))
+(let ((product 1))
+  (loop with n = 3
+        repeat n
+        for a = (read)
+        do (setf product (* product a))
+        finally (format t "~A~%" product)))
