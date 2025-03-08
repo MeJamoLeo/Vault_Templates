c447dbf46f3ade073a30817c3214ef5d66724e95
treo <treo@treo.treo>
2025-03-08 06:46:43 -0600

    vault backup: 2025-03-08 06:46:43

--- 99_Projects/AtCoder/Mathematics X Algorithm/codes/003_Sum_of_N_Integers.lisp
+++ 99_Projects/AtCoder/Mathematics X Algorithm/codes/003_Sum_of_N_Integers.lisp
@@ -1,6 +1,6 @@
 (let ((n (read)))
   (loop with ans = 0
-		repeat n
-		for a = (read)
-		do (+ ans a)
-		finally (format t "~A~%" ans)))
+        repeat n
+        for a = (read)
+        do (incf ans a)
+        finally (format t "~A~%" ans)))
