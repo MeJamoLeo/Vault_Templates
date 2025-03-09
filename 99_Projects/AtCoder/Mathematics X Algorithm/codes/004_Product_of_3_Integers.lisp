eca57621074910b903bdf1c59f47dc97eb640814
treo <treo@treo.treo>
2025-03-08 22:46:23 -0600

    vault backup: 2025-03-08 22:46:23

--- 99_Projects/AtCoder/Mathematics X Algorithm/codes/004_Product_of_3_Integers.lisp
+++ 99_Projects/AtCoder/Mathematics X Algorithm/codes/004_Product_of_3_Integers.lisp
@@ -2,3 +2,9 @@
        (loop with n = 3 repeat n for a = (read)
              do (setf product (* product a))
              finally (format t "~A~%" product)))
+
+(loop for i
+    from 0
+    for day
+    in '(monday tuesday wednesday thursday friday saturday sunday)
+    collect (cons i day))
